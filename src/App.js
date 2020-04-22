9101c999890dca9eda72a3a4f9ad5c6ad700571f
Porson <porsonlee@gmail.com>
2020-04-21 19:12:42 -0700

    temporarily set up container with one main component displaying HOT or COLD as it's a game of guessing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Welcome to our react app</h1>
-    </div>
-  );
+import { Grid, Typography, Paper, Divider } from '@material-ui/core';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Grid container style={{ height: '100vh' }} justify="center" alignitems="center">
+        <Grid item xs={3}>
+          <Paper style={{padding: '50px' }} elevation={6}>
+            <Typography align="center" variant="h2" gutterBottom>HOT or COLD</Typography>
+            <Divider style={{ margin: '20px 0' }} />
+          </Paper>
+        </Grid>
+      </Grid>
+    );
+  }
 }
 
 export default App;
