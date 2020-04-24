9d90324832fdcbf49d091c129ae0812233b73635
Porson <porsonlee@gmail.com>
2020-04-23 23:55:44 -0700

    implementing guess functions:

--- src/App.js
+++ src/App.js
@@ -5,15 +5,38 @@ import { Grid, Typography, Paper, Divider } from '@material-ui/core';
 import { generateRandomNumber } from "./util";
 
 import Form from "./components/Form";
+import Progress from "./components/Progress";
 
 class App extends React.Component {
 
+  state = {
+    generatedNumber: generateRandomNumber(),
+    guess: undefined,
+    allGuess: [],
+    attempt: 0,
+  }
+
   updateAppState = (guess) => {
-    console.log(generateRandomNumber())
-    console.log(guess)
+
+    const absDiff = Math.abs(guess - this.state.generatedNumber);
+
+    this.setState(prevState => ({
+      guess,
+      allGuesses: [ ...prevState.allGuesses, { guess } ],
+      attempt: prevState.attempt +1
+    }))
   }
 
   render() {
+
+    const { allGuesses, attempt } = this.state; 
+
+    const guessList = allGuesses.map((item, index) => (
+      <li key={index}>
+        <span>{item.guess}</span>
+      </li>
+    ))
+
     return (
       <Grid container style={{ height: '100vh' }} justify="center" alignitems="center">
         <Grid item xs={3}>
@@ -21,6 +44,7 @@ class App extends React.Component {
             <Typography align="center" variant="h2" gutterBottom>HOT or COLD</Typography>
             <Divider style={{ margin: '20px 0' }} />
             <Form returnGuessToApp={guess => this.updateAppState(guess)} />
+            <Progress attempt={attempt} guessList={guessList}></Progress>
           </Paper>
         </Grid>
       </Grid>
--- src/components/Progress.js
+++ src/components/Progress.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 import { Typography } from "@material-ui/core";
 
-const Progress = () => {
+const Progress = ({ attempt, guessList }) => (
   <div>
     <Typography variant="h2"> Geuss # {attempt}</Typography>
     <ul className="progressBar-history">
       {guessList}
     </ul>
   </div>
-}
+);
 
 export default Progress;
